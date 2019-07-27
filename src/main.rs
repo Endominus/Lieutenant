fbc2946d59002bd41f7bf9aa9a0e0039e6bc569e
Endominus <endominus@gmail.com>
2019-07-27 16:18:58 +0400

    Switched to reqwest, that worked, and a bunch of database ORMs that didn't. Still testing.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-extern crate cursive;
+// extern crate cursive;
 
 // use cursive::Cursive;
 // use cursive::views::{Dialog, TextView};
@@ -6,41 +6,49 @@ extern crate cursive;
 // use std::io::{self, Write};
 // use std::collections::HashMap;
 
-// use std::io::Read;
-// extern crate reqwest;
-
-// fn main() -> Result<(), Box<std::error::Error>> {
-//     let mut res = reqwest::get("https://api.magicthegathering.io/v1/cards?name=Griselbrand")?;
-//     let mut body = String::new();
-//     res.read_to_string(&mut body)?;
-
-//     println!("Status: {}", res.status());
-//     println!("Headers:\n{:#?}", res.headers());
-//     println!("Body:\n{}", body);
-//     Ok(())
-// }
-
-// #[macro_use]
-extern crate ureq;
+use std::io::Read;
+extern crate reqwest;
 extern crate json;
+extern crate rusqlite;
 
-fn main() {
+fn main() -> Result<(), Box<std::error::Error>> {
+    let mut res = reqwest::get("https://api.magicthegathering.io/v1/cards?name=Griselbrand")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+    let parsed = json::parse(&body).unwrap();
 
-    // sync post request of some json.
-    let mut request = ureq::get("https://api.magicthegathering.io/v1/cards");
-    let response = request.query("name", "Griselbrand").call();
-    let text = response.into_json().unwrap();
-    let null = &text["cards"][101];
-    let mut c = 0;
-    let mut card = &text["cards"][c];
-    while (card != null) & (c < 20) {
+    // println!("Status: {}", res.status());
+    // println!("Headers:\n{:#?}", res.headers());
+    // println!("Body:\n{}", parsed["cards"]);
+    
+
+    for card in parsed["cards"].members() {
         println!("Name: {}", card["name"]);
-        println!("Mana Cost: {}", card["manaCost"]);
-        println!("Text: {}", card["text"]);
-        c += 1;
-        card = &text["cards"][c];
     }
-    // for card in text["cards"].iter() {
-
-    // }
+    Ok(())
 }
+
+// // #[macro_use]
+// extern crate ureq;
+// extern crate json;
+
+// fn main() {
+
+//     // sync post request of some json.
+//     let mut request = ureq::get("https://api.magicthegathering.io/v1/cards");
+//     let response = request.query("name", "Griselbrand").call();
+//     let text = response.into_json().unwrap();
+//     let null = &text["cards"][101];
+//     let mut c = 0;
+//     let mut card = &text["cards"][c];
+//     while (card != null) & (c < 20) {
+//         println!("Name: {}", card["name"]);
+//         println!("Mana Cost: {}", card["manaCost"]);
+//         println!("Text: {}", card["text"]);
+//         c += 1;
+//         card = &text["cards"][c];
+//     }
+//     // for card in text["cards"].iter() {
+
+//     // }
+// }
