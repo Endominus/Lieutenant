2b358ccb801d7d99bf6aacb9d88c90fbe2597238
Endominus <endominus@gmail.com>
2019-10-05 11:38:10 +0400

    Imported cursive successfully
    Moved the retrieval code to a separate module.
    
    May have to move it to a new file to manage the errors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,83 @@
 extern crate rusqlite;
+extern crate reqwest;
+
 use rusqlite::types::ToSql;
 use rusqlite::{params, Connection, Result};
 
+use std::error::Error;
+
+use reqwest::Client;
+use reqwest::Response;
 
 #[derive(Debug)]
-struct Card {
-    name: String,
-    types: Vec<String>,
-    subtypes: Vec<String>,
-    text: String,
-    cmc: i8,
-    mana_cost: String,
-    color_identity: Vec<char>,
-    related_cards: Vec<String>
+pub struct Card {
+    pub name: String,
+    pub types: Vec<String>,
+    pub subtypes: Vec<String>,
+    pub text: String,
+    pub cmc: i8,
+    pub mana_cost: String,
+    pub color_identity: Vec<char>,
+    pub related_cards: Vec<String>
 }
 
 impl Card {
-    // fn new(name String, types)
+    fn new(
+        name: String, 
+        types: Vec<String>, 
+        subtypes: Vec<String>,
+        text: String,
+        cmc: i8,
+        mana_cost: String,
+        color_identity: Vec<char>,
+        related_cards: Vec<String>) -> Card {
+            Card {
+                name,
+                types,
+                subtypes,
+                text,
+                cmc,
+                mana_cost,
+                color_identity,
+                related_cards
+            }
+        }
+}
+
+fn run() {
+    
+}
+
+mod Online {
+    fn retrieve_card_by_name(name: String) -> Result<super::Card, &'static str> {
+        let mut res = reqwest::get("https://api.magicthegathering.io/v1/cards?name=Avacyn").unwrap();
+        let mut body = String::new();
+        res.text().unwrap();
+        let parsed = json::parse(&body).unwrap();
+
+        // println!("Status: {}", res.status());
+        // println!("Headers:\n{:#?}", res.headers());
+        // println!("Body:\n{}", parsed["cards"]);
+
+        let mut seen: Vec<&json::JsonValue> = Vec::new();
+        
+
+        for card in parsed["cards"].members() {
+            if seen.contains(&&card["name"]) {
+                continue;
+            }
+            seen.push(&card["name"]);
+            println!("Name: {}", card["name"]);
+            println!("Supertypes: {}", card["supertypes"]);
+            println!("Types: {}", card["types"]);
+            println!("Subtypes: {}", card["subtypes"]);
+            println!("Text: {}", card["text"]);
+            println!("Cmc: {}", card["cmc"]);
+            println!("ColorIdentity: {}", card["colorIdentity"]);
+            println!("Names: {}", card["names"]);
+            println!("Mana Cost: {}\n", card["manaCost"]);
+        }
+        // Ok(())
+        panic!()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// extern crate cursive;
 
 // use cursive::Cursive;
 // use cursive::views::{Dialog, TextView};
@@ -6,43 +5,111 @@
 // use std::io::{self, Write};
 // use std::collections::HashMap;
 
+
 extern crate json;
 extern crate reqwest;
+extern crate cursive;
 
 
-use std::io::Read;
+// use std::io::Read;
 
+use cursive::event::Key;
+use cursive::menu::MenuTree;
+use cursive::traits::*;
+use cursive::views::Dialog;
+use cursive::Cursive;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut res = reqwest::get("https://api.magicthegathering.io/v1/cards?name=Avacyn")?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
-    let parsed = json::parse(&body).unwrap();
+fn main() {
+    // Creates the cursive root - required for every application.
+    let mut siv = Cursive::default();
 
-    // println!("Status: {}", res.status());
-    // println!("Headers:\n{:#?}", res.headers());
-    // println!("Body:\n{}", parsed["cards"]);
+    // We'll use a counter to name new files.
+    let counter = AtomicUsize::new(1);
 
-    let mut seen: Vec<&json::JsonValue> = Vec::new();
-    
+    // The menubar is a list of (label, menu tree) pairs.
+    siv.menubar()
+        // We add a new "File" tree
+        .add_subtree(
+            "File",
+            MenuTree::new()
+                .leaf("New Deck", move |s| {})
+                // .subtree("Open Deck", 
+                // Trees are made of leaves, with are directly actionable...
+                .leaf("New", move |s| {
+                    // Here we use the counter to add an entry
+                    // in the list of "Recent" items.
+                    let i = counter.fetch_add(1, Ordering::Relaxed);
+                    let filename = format!("New {}", i);
+                    s.menubar()
+                        .find_subtree("File")
+                        .unwrap()
+                        .find_subtree("Recent")
+                        .unwrap()
+                        .insert_leaf(0, filename, |_| ());
 
-    for card in parsed["cards"].members() {
-        if seen.contains(&&card["name"]) {
-            continue;
-        }
-        seen.push(&card["name"]);
-        println!("Name: {}", card["name"]);
-        println!("Supertypes: {}", card["supertypes"]);
-        println!("Types: {}", card["types"]);
-        println!("Subtypes: {}", card["subtypes"]);
-        println!("Text: {}", card["text"]);
-        println!("Cmc: {}", card["cmc"]);
-        println!("ColorIdentity: {}", card["colorIdentity"]);
-        println!("Names: {}", card["names"]);
-        println!("Mana Cost: {}\n", card["manaCost"]);
-    }
-    Ok(())
+                    s.add_layer(Dialog::info("New file!"));
+                })
+                // ... and of sub-trees, which open up when selected.
+                .subtree(
+                    "Recent",
+                    // The `.with()` method can help when running loops
+                    // within builder patterns.
+                    MenuTree::new().with(|tree| {
+                        for i in 1..100 {
+                            // We don't actually do anything here,
+                            // but you could!
+                            tree.add_leaf(format!("Item {}", i), |_| ())
+                        }
+                    }),
+                )
+                // Delimiter are simple lines between items,
+                // and cannot be selected.
+                .delimiter()
+                .with(|tree| {
+                    for i in 1..10 {
+                        tree.add_leaf(format!("Option {}", i), |_| ());
+                    }
+                }),
+        )
+        .add_subtree(
+            "Help",
+            MenuTree::new()
+                .subtree(
+                    "Help",
+                    MenuTree::new()
+                        .leaf("General", |s| {
+                            s.add_layer(Dialog::info("Help message!"))
+                        })
+                        .leaf("Online", |s| {
+                            let text = "Google it yourself!\n\
+                                        Kids, these days...";
+                            s.add_layer(Dialog::info(text))
+                        }),
+                )
+                .leaf("About", |s| {
+                    s.add_layer(Dialog::info("Cursive v0.0.0"))
+                }),
+        )
+        .add_delimiter()
+        .add_leaf("Quit", |s| s.quit());
+
+    // When `autohide` is on (default), the menu only appears when active.
+    // Turning it off will leave the menu always visible.
+    // Try uncommenting this line!
+
+    // siv.set_autohide_menu(false);
+
+    siv.add_global_callback(Key::Esc, |s| s.select_menubar());
+
+    siv.add_layer(Dialog::text("Hit <Esc> to show the menu!"));
+
+    siv.run();
 }
+
+
+
+
 
 // // #[macro_use]
 // extern crate ureq;
