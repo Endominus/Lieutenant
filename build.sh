e230f3a77df5debd737b15c308bf82bce41c1833
Endominus <endominus@gmail.com>
2022-01-03 14:34:41 +0400

    Changed some of the build files.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 cargo build --release --target x86_64-unknown-linux-gnu
 tar -czvf Lieutenant-v$argv-x86_64-unknown-linux-gnu.tar.gz -C artifacts lieutenant.db settings.toml -C ../target/x86_64-unknown-linux-gnu/release lieutenant
 cargo build --release --target x86_64-pc-windows-gnu
-tar -cavf Lieutenant-v$argv-x86_64-pc-windows-msvc.zip -C artifacts lieutenant.db settings.toml -C ../target/x86_64-pc-windows-gnu/release lieutenant.exe
+tar -cavf Lieutenant-v$argv-x86_64-pc-windows-gnu.zip -C artifacts lieutenant.db settings.toml -C ../target/x86_64-pc-windows-gnu/release lieutenant.exe
